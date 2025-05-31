bf9c8fcb8a9d340f0bf93635c1daa272602080c4
Kim-Yukyung <yukyung116@gmail.com>
2025-05-31 23:29:36 +0900

    ✨ Morama help 실행 시 고정된 형식의 도움말 출력

--- cmd/help.go
+++ cmd/help.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -13,15 +12,27 @@ import (
 // helpCmd represents the help command
 var helpCmd = &cobra.Command{
 	Use:   "help",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "도움말 보기",
+	Long:  "모든 명령어와 사용법을 요약해서 출력합니다.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("help called")
+		fmt.Println(`morama 0.1.0
+영화·드라마 기록 CLI
+
+Usage:
+  morama [command]
+
+Available Commands:
+  add         기록 추가
+  delete      기록 삭제
+  edit        기록 수정
+  list        기록 목록보기
+  show        상세 후기 보기
+  report      연도/유형별 통계 출력
+  help        도움말
+
+Flags:
+  -h, --help     help for morama
+      --version  버전 정보 출력`)
 	},
 }
 
