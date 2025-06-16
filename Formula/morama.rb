c7a6cbf97fb7a1e513fbe7b544b5f8f30ac7cbde
kiku99 <elsd0326@gmail.com>
2025-06-16 21:32:37 +0900

    ci: update-homebrew.yml

--- Formula/morama.rb
+++ Formula/morama.rb
@@ -1,18 +1,18 @@
 class Morama < Formula
-  desc "Morama CLI tool"
+  desc "A CLI tool for managing your watched movies and dramas"
   homepage "https://github.com/kiku99/morama"
   url "https://github.com/kiku99/morama/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "YOUR_TARBALL_SHA256"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   version "v1.0.0"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/morama"
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "."
   end
 
   test do
-    system "#{bin}/morama", "--version"
+    system "#{bin}/morama", "version"
   end
 end 
