16b0755d5eb3d3e2059c1e109a96fee3749729f5
kiku99 <elsd0326@gmail.com>
2025-06-18 10:46:06 +0900

    chore: update version.go
    1. version 동적 주입
    2. 명령어 설명 수정

--- cmd/version.go
+++ cmd/version.go
@@ -9,15 +9,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "0.1.0" // 기본값
+var (
+	version = "dev"     // default value
+	commit  = "unknown" // Git commit hash
+	date    = "unknown" // build date
+)
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "버전 정보를 출력합니다",
+	Short: "Show version information",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("morama version", version)
+		fmt.Printf("morama version %s\n", version)
+		fmt.Printf("Git commit: %s\n", commit)
+		fmt.Printf("Built: %s\n", date)
 	},
 }
 
